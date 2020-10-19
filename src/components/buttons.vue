6a12265342f8982d7277afeb5892030d9a107f94
jfbee <xomjachok13@gmail.com>
2020-10-19 11:04:41 +0300

    TVP-1 change links; add buttons component

---
+++ src/components/buttons.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    <v-list-item>
+      <v-list-item-content>
+        <v-btn color = "#006675">Кнопка 1</v-btn>
+        <v-btn color = "#8c4d30">Кнопка 2</v-btn>
+        <v-btn color = "#662e38">Кнопка 3</v-btn>
+        <v-btn color = "#8c4d30">Кнопка 4</v-btn>  
+        <v-btn color = "#006675">Кнопка 5</v-btn>
+        <v-btn color = "#662e38">Кнопка 6</v-btn>
+      </v-list-item-content>
+    </v-list-item>
+    </div>
+    
+</template>
