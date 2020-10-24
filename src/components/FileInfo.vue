00f683eac3ec71b82665db963a12c6fba348f9d7
jfbee <xomjachok13@gmail.com>
2020-10-24 20:52:07 +0300

    TVP-7 add hash SHA256 and SHA1

--- src/components/FileInfo.vue
+++ src/components/FileInfo.vue
@@ -1,46 +1,40 @@
 <template>
-    <div class="content">
+<div class="content">
         <input type="file" id="file" ref="file" v-on:change="handleFileUpload"/>
 
         <div class="info"
-        v-if="isUploadFile"
+        v-if="typeof fileFullName === 'string'"
         >
             <p>Full file name : [ {{fileFullName}} ]</p>
             <p>File name : [ {{fileName}} ]</p>
             <p>File type : [ {{fileExtencion}} ]</p>
+            <p>SHA256 :<br> {{printHashSHA256()}}</p>
+            <p>SHA1 :<br> {{printHashSHA1()}}</p>
         </div>
 
-
-
-        <div 
-        v-if="isLoadingFile"
-        >
-
-        <v-progress-circular class="loader"
-        :size="70"
-        :width="7"
-        color="purple"
-        indeterminate
-        ></v-progress-circular>
-
+        <div class="loader">
+        <Loader v-if="isLoading"/>
         </div>
 
+        
 
 
 
-    </div>
+
+        
+</div>
 </template>
 
 
 
 
 <script>
+import Loader from './Loader'
 export default {
     data () {
         return {
-            isUploadFile: false,
-            isLoadingFile: false,
-            fileFullName: '',
+            isLoading: true,
+            fileFullName: null,
             fileName: '',
             fileExtencion: '', 
 
@@ -49,7 +43,7 @@ export default {
 
     methods: {
         handleFileUpload(){
-            this.isLoadingFile = true;
+            this.isLoading = false;
 
             this.fileFullName = this.$refs.file.files[0].name;
             
@@ -58,8 +52,7 @@ export default {
             this.fileName = this.fileFullName.replace(this.fileExtencion,'') ;
             
             this.sleep(2000);
-            this.isUploadFile = true;
-            this.isLoadingFile = false;
+            this.isLoading = true;
             
         },
         sleep(millis) {
@@ -69,40 +62,52 @@ export default {
                 i = i + 1;
             }
         },
+        
+        printHashSHA256(){
+            var sha256 = require('tiny-sha256');
+            var hash = sha256(this.fileFullName);
+            return hash;
+        },
+
+        printHashSHA1()
+        {
+            var sha1 = require('js-sha1');
+            var hash = sha1(this.fileFullName);
+            return hash;
+        }
 
     },
+    components:{
+        Loader,
+    }
 }
 </script>
 
 
 <style scoped>
 .content{
-    width: 400px;
-    height: 400px;
+    width: 600px;
+    height: 500px;
+    text-align: center;
     position: relative;
     margin: 0 auto;
     margin-top: 5%;
     border: solid 2px black;
+    background-color: #f0f0f0;
 }
 
 .info{
-    width: 300px;
-    height: 300px;
+    width: 500px;
+    height: 400px;
     position: relative;
     margin: 0 auto;
     margin-top: 3%;
     text-align: left;
-    padding-left: 30px;
     border: solid 2px blue;
+    background-color: #f5f5f5;
 }
 
-.buttontest{
-    text-align: right;
-    margin-top: 1%;
-    margin-left: 60%;
-}
 .loader{
     margin-top: 30%;
-    margin-left: 40%;
 }
 </style>
